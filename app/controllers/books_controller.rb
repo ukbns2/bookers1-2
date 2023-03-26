d9d29b3541fbe85f23bc41dc9010755d3a048f6a
ukbns2 <110463303+ukbns2@users.noreply.github.com>
2023-03-26 07:23:38 +0000

    bookのcreate機能の作成完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,18 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+   @book = Book.new(book_params)
+   if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+   else
+     @books = Book.all
+     render :index
+   end
+  end
+
+
   def index
     @books = Book.all
     @book = Book.new
@@ -11,5 +23,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
